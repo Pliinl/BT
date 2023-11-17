2b1d93ea7ca132fe23a33ebc608242e19d0ed099
Pliinl <lehongducplinl@gmail.com>
2023-11-17 18:37:06 +0700

    Bài Tập

--- BT 14-11-2023/practice-3.js
+++ BT 14-11-2023/practice-3.js
@@ -1,8 +1,15 @@
-// Viết hàm solveEquation() để giải phương trình 
+// Viết hàm solveEquation() để giải phương trình
 // bậc nhất ax + b = 0 với a và b là 2 tham số đầu vào.
 function solveEquation(a, b) {
-     x = -b / a;
-    console.log(x);
+  if (a == 0) {
+    if (b == 0) {
+      console.log("Phương trình có vô số nghiệm");
+    } else {
+      console.log("Phương trình vô nghiệm");
+    }
+  } else {
+    console.log(`Phươngg trình có nghiệm duy nhất x = ${-b / a}`);
+  }
 }
 
 solveEquation(2, -4);
---
+++ BT 16-11-2023/practice-3.js
@@ -0,0 +1,26 @@
+/**
+ * Viết hàm isPrimeNumber() nhận vào một số nguyên và kiểm tra xem đó có phải là số nguyên tố hay không
+ * Nếu có, trả về true. Ngược lại, trả về false.
+ *
+ * Ví dụ:
+ * isPrimeNumber(4)
+ *
+ * Output:
+ * false
+ *
+ * */
+
+function isPrimeNumber(number) {
+  if (number <= 1) {
+    return false;
+  } else {
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+console.log(isPrimeNumber(4));
